3c46678c488131baad0d90e4381ab635d34ad2b9
Nicolas Caqueux <36823067+semi-corpus@users.noreply.github.com>
2019-05-20 16:56:19 +0200

    Grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,41 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
+  pages: any;
+
   constructor(
+    private router: Router,
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    this.pages = [
+      {name: 'home', title: 'Accueil'},
+      {name: 'grid', title: 'Grid'}
+    ]
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }      
+  
+  selectPage(name: string) {
+    this.router.navigateByUrl('/' + name);
+    this.menu.close()
   }
 }
